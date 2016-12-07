1344f9f6b64fa08653cd9e5782d7186174631566
Alexander Wenzel <elkangaroo@gmail.com>
2016-12-07 23:17:17 +0100

    day 6: add implementation of signal decoder

--- src/Day6/Signals.php
+++ src/Day6/Signals.php
@@ -10,7 +10,25 @@ class Signals
      */
     public function decode($input)
     {
-        return '';
+        $signals = [];
+
+        foreach (explode(PHP_EOL, $input) as $line) {
+            $signals[0] .= $line[0];
+            $signals[1] .= $line[1];
+            $signals[2] .= $line[2];
+            $signals[3] .= $line[3];
+            $signals[4] .= $line[4];
+            $signals[5] .= $line[5];
+        }
+
+        $decoded = array_map(function($line) {
+            $chars = count_chars($line, 1);
+            arsort($chars);
+
+            return chr(key($chars));
+        }, $signals);
+
+        return implode('', $decoded);
     }
 }
 
